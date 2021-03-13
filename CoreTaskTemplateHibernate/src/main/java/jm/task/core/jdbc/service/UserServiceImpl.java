59d7a1303c715109f872cb4e44d1be3ad1fcf71f
filatovevgeny13 <76839320+filatovevgeny13@users.noreply.github.com>
2021-03-13 13:53:06 +0500

    Add files via upload

---
+++ CoreTaskTemplateHibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private final UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
+
+    public void createUsersTable() throws SQLException {
+        dao.createUsersTable();
+    }
+
+    public void dropUsersTable() throws SQLException {
+        dao.dropUsersTable();
+    }
+
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        dao.saveUser(name, lastName, age);
+        System.out.println("User с именем " + name + " добавлен в базу данных");
+    }
+
+    public void removeUserById(long id) throws SQLException {
+        dao.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        return dao.getAllUsers();
+    }
+
+    public void cleanUsersTable() throws SQLException {
+        dao.cleanUsersTable();
+    }
+}
